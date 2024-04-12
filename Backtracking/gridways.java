8dc75327ac867643ef5de51ff6e1e9e5c0264aa5
ujjwal sinha <115147132+Ujjwal-sinha@users.noreply.github.com>
2024-04-12 20:21:22 +0530

    Add files via upload

---
+++ Backtracking/gridways.java
@@ -0,0 +1,24 @@
+package Backtracking;
+
+public class gridways {
+    public static int ways(int i, int j, int n, int m) {
+
+        if (i == n - 1 && j == m - 1) {
+            return 1;
+
+        } else if (i == n || j == n) {
+            return 0;
+
+        }
+
+        int w1 = ways(i + 1, j, n, m);
+        int w2 = ways(i, j + 1, n, m);
+        return w1 + w2;
+    }
+
+    public static void main(String[] args) {
+        int n = 3, m = 3;
+        System.out.println(ways(0, 0, n, m));
+
+    }
+}
